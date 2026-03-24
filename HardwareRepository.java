7610c61125fef07a2acfe167090aee3eae129ea3
santinoarevalo66-hash <santinoarevalo66@gmail.com>
2026-03-24 21:49:46 +0800

    Update HardwareRepository.java

--- HardwareRepository.java
+++ HardwareRepository.java
@@ -1,1 +1,12 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class HardwareRepository {
+    private List<Hardware> hardwareList;
+    private static final String DB_URL = "jdbc:sqlite:hardware.db";
+
+    public HardwareRepository() {
+        hardwareList = new ArrayList<>();
+        initializeDatabase();
+    }
