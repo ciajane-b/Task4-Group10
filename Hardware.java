f630ffe8a07a437fc2607b45e6811d68633a8b19
AldCream <aldrianmaure@gmail.com>
2026-03-24 21:33:04 +0800

    Add abstract Hardware class with properties and methods

--- Hardware.java
+++ Hardware.java
@@ -1,1 +1,38 @@
+public abstract class Hardware {
+    protected int id;
+    protected String brand;
+    protected String model;
+    protected int specValue;
+
+    public Hardware(int id, String brand, String model, int specValue) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.specValue = specValue;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSpecValue() {
+        return specValue;
+    }
+
+    public abstract String getSpecDisplay();
+
+    @Override
+    public String toString() {
+        return String.format("| %-3d | %-12s | %-15s | %-15s |",
+                id, brand, model, getSpecDisplay());
+    }
+}
 
